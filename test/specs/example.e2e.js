d9ae14118b1cccf8db1f3090d8d9c3c683178a65
alexandershumakov <alexander.shumakov@gmail.com>
2022-12-18 00:15:54 +0200

    second commit

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,20 +1,20 @@
 
-describe('My Login application', () => {
-    before( () => {
-        browser.maximizeWindow();
-    })
-
-
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`)
-
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
-
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!')
-    })
-})
+// describe('My Login application', () => {
+//     before( () => {
+//         browser.maximizeWindow();
+//     })
+//
+//
+//     it('should login with valid credentials', async () => {
+//         await browser.url(`https://the-internet.herokuapp.com/login`)
+//
+//         await $('#username').setValue('tomsmith')
+//         await $('#password').setValue('SuperSecretPassword!')
+//         await $('button[type="submit"]').click()
+//
+//         await expect($('#flash')).toBeExisting()
+//         await expect($('#flash')).toHaveTextContaining(
+//             'You logged into a secure area!')
+//     })
+// })
 
--- test/specs/example.js
+++ test/specs/example.js
@@ -1,12 +1,72 @@
 import { expect } from "chai";
 import expected from './../../data/expected.json'
 
-describe('QA Project', () => {
+describe('User registers into the system', () => {
     before(() => {
         browser.maximizeWindow();
         browser.url('/');
     })
-    it('Should verify', async () => {
+
+    it('Check title', async () => {
+        const registerLink = await $("//ul//a[@tabindex ='9']")
+        await registerLink.click()
         expect(await browser.getTitle()).to.equal(expected.titleName)
     })
+
+    it('Fill all fields and finish', async () => {
+        const languageDropdown = await $("//input[@id='tuserName']")
+        const password = await $("//input[@id='tPassword']")
+        const passwordReType = await $("//input[@id='tPasswordRetype']")
+        const firstQuestionDropdown = await $("//select[@id='ssec1']")
+        const yourAnswerField = await $("//input[@id='tsecAnswer1']")
+        const retypeAnswerField = await $("//input[@id='tsecAnswer1Match']")
+        const secondQuestionDropdown = await $("//select[@id='ssec2']")
+        const yourAnswerField2 = await $("//input[@id='tsecAnswer2']")
+        const retypeAnswerField2 = await $("//input[@id='tsecAnswer2Match']")
+        const account1RadioButton = await $("//label[@id='for-rAccount1']")
+        const title = await $("//select[@id='stitle']")
+        const firstName = await $("//input[@id='tfName']")
+
+
+        await languageDropdown.setValue("shumm4ik")
+        await password.setValue("Qw12345@")
+        await passwordReType.setValue("Qw12345@")
+        await firstQuestionDropdown.selectByVisibleText("What is the name of your pet?")
+        await yourAnswerField.setValue("test1")
+        await retypeAnswerField.setValue("test1")
+        await secondQuestionDropdown.selectByVisibleText("In what city were you born?")
+        await yourAnswerField2.setValue("test2")
+        await retypeAnswerField2.setValue("test2")
+        await account1RadioButton.click()
+        await title.selectByVisibleText("Mr")
+        await firstName.setValue("Odin")
+
+        await browser.pause(3000)
+
+    })
+    //
+    // it('Should verify that login input field accept valid data', () => {
+    //     const element = "//input[@id='user-name']";
+    //     const loginName = 'standard_user';
+    //     expect($(element).setValue(loginName));
+    // })
+    //
+    // it('Should verify that login input field accept valid data', () => {
+    //     const elementPassword = "//input[@id='password']";
+    //     const password = 'secret_sauce';
+    //     expect($(elementPassword).setValue(password));
+    // })
+    //
+    // it('Should verify that login input field accept valid data', () => {
+    //     const singInButton = "//input[@id='login-button']";
+    //     $(singInButton).click();
+    // })
+    //
+    // it('Should verify that login input field accept valid data', async () => {
+    //     await browser.setTimeout({ 'implicit': 5000 })
+    //     const products = await $("//span[contains(text(),'Products')]");
+    //     await expect($(products).getText()).to.equal("Products");
+    // })
+
+
 })
